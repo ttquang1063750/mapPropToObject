f4eed7f910ed782844b18de40e15dd6775eddb3c
Tang Thanh Quang <ttquang1063750@gmail.com>
2021-06-09 10:56:49 +0700

    deploy base-href

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
+const { BaseHrefWebpackPlugin } = require('base-href-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
+    plugins: [
+        new BaseHrefWebpackPlugin({ baseHref: '/mapPropToObject/' })
+    ]
 });
